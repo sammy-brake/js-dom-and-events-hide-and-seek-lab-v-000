a47c6b6f0e75ea617fc15619de5c0a209971e10b
Samantha brake <sammy.brake@gmail.com>
2019-05-23 00:22:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ function nestedTarget(nested, target) {
 
 function increaseRankBy(n) {
   let rankedList = document.querySelectorAll(.ranked-list);
+  for (let i = 0; i < rankedList.length; i++) {
+    let currentN = parseInt(rankedList[i]);
+    let addN = parseInt(n);
+    rankedList[i] = currentN + addN;
+  }
 };
+
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}
 
 function deepestChild() {
 
