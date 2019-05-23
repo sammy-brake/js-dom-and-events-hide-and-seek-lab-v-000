e732ceb8929ef4ffb4bf8e3fdd85df03fe14eaef
Samantha brake <sammy.brake@gmail.com>
2019-05-23 00:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(nested, target) {
 function increaseRankBy(n) {
   let rankedList = document.querySelectorAll(.ranked-list);
   for (let i = 0; i < rankedList.length; i++) {
-    let currentN = parseInt(rankedList[i]);
+    let currentN = parseInt(rankedList[i].innerHTML);
     let addN = parseInt(n);
     rankedList[i].innerHTML = currentN + addN;
   };
